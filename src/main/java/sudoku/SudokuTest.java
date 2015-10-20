98adbabeb8acd1abd4d65e3fc9f4bde55b34e11e
DongHoon <dhdnzk0304@gmail.com>
2015-10-20 17:15:42 +0900

    메인메소드 있는 테스트 클래스 추가(추가해야 할 내용들 주석으로 메소드 안에 달아둠

---
+++ src/main/java/sudoku/SudokuTest.java
@@ -0,0 +1,14 @@
+package sudoku;
+
+public class SudokuTest {
+    public static void main ( String[] args ) {
+        Problem p1 = new Problem();
+        // showState( p1 );
+        while ( 0 < p1.numOfZero ) {
+            // juniorExplanation( p1 );
+            // seniorExplanation( p1 );
+            // showstate( p1 );
+        }
+        // showstate( p1 );
+    }
+}
