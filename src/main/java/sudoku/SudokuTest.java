606fde0d1773ccb1efb861335786972294393583
master <dhdnzk0304@gmail.com>
2017-05-01 17:34:21 +0900

    리팩토링 시작

--- src/main/java/sudoku/SudokuTest.java
+++ src/main/java/sudoku/SudokuTest.java
@@ -2,18 +2,14 @@ package sudoku;
 
 public class SudokuTest {
     public static void main ( String[] args ) {
-        Problem p1 = new Problem( MakingQuestion.makeQuiz() );
-        p1.showState();
-        while ( 0 < p1.numOfZero ) {
-            int before = p1.getNumOfZero();
-            SolveSingleCandidate.solve( p1 );
-            SolveHiddenSingleCandidate.solve( p1 );
-            int cur = p1.getNumOfZero();
-            if ( before == cur ) {
-                System.out.println("현재 구현된 풀이법으로는 더이상 풀 수 없습니다");
-                return;
-            }
-        }
-        p1.showState();
+
+        String str = "010004000408000901090701245860030704000508000107020089576302090209000107000800050";
+
+        ProblemProvider gameBoard = ProblemProvider.getInstance();
+
     }
 }
+
+// TODO 파일 읽어들이는 클래스 만들기.(싱글톤) 텍스트파일에서 읽어오기 or int[]형 1차원 배열 or 2차원 배열 or 스트링
+// TODO static으로 구현된 클래스들 인스턴스 생성으로 리팩토링
+// TODO 고급 풀이법 구현
