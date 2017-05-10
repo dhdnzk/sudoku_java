392e41a3d57b985db3f20a4efa95483ff49fe83b
Donghoon Im <dongttang@gmail.com>
2017-05-10 13:14:56 +0900

    클래스 접근범위 수정, abstract처리

--- src/main/java/sudoku/SeniorExplanation.java
+++ src/main/java/sudoku/SeniorExplanation.java
@@ -1,6 +1,6 @@
 package sudoku;
 
-public class SeniorExplanation extends JuniorExplanation {
+abstract class SeniorExplanation extends JuniorExplanation {
     // TODO 한줄이나 한 칸에 있는 후보자 수
     // TODO 두줄이나 두 칸에 있는 후보자 수
     // TODO 노출되어있는 두개의 순환수
