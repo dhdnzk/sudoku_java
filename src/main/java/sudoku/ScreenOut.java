38d11532029a5ca61318d83335a8e6f9e7ebed51
Donghoon Im <dongttang@gmail.com>
2017-05-10 13:13:47 +0900

    showState 메소드 구현

--- src/main/java/sudoku/ScreenOut.java
+++ src/main/java/sudoku/ScreenOut.java
@@ -26,4 +26,73 @@ class ScreenOut {
                 System.out.println("------------------------------------");
         }
     }
+
+    static void showBoard(GameBoardElement[][] board) {
+
+        System.out.println("------------------------------------");
+        for(int i = 0; i < 9; i ++) {
+            System.out.print("|");
+            for(int j = 0; j < 9; j ++) {
+                if(board[i][j].getConfirmedElement() == 0) {
+                    System.out.print("\t");
+                }
+                else {
+                    System.out.print(" " + board[i][j] + "\t");
+                }
+                if((j+1)%3 == 0)
+                    System.out.print("|");
+            }
+            System.out.println();
+            if((i+1)%3 == 0)
+                System.out.println("------------------------------------");
+        }
+
+
+    }
+
+    static void showState(GameBoardElement[][] gameBoard) {
+
+        for (int row = 0; row < gameBoard.length; row++) {
+            if ( row % 3 == 0 && row > 1 )
+                System.out.println( "━━━━━━━━━━━━━━━━" );
+            for (int col = 0; col < gameBoard[row].length; col++) {
+                if ( col % 3 == 0 && col > 1 )
+                    System.out.print( "┃ " );
+                if (gameBoard[row][col].getConfirmedElement() != 0) {
+                    System.out.print( gameBoard[row][col].getConfirmedElement() + " " );
+                }
+                else
+                    System.out.print("  ");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n\n");
+    }
+
+    static void showState(int[][] gameBoard) {
+
+        for (int row = 0; row < gameBoard.length; row++) {
+            if ( row % 3 == 0 && row > 1 )
+                System.out.println( "━━━━━━━━━━━━━━━━" );
+            for (int col = 0; col < gameBoard[row].length; col++) {
+                if ( col % 3 == 0 && col > 1 )
+                    System.out.print( "┃ " );
+                if (gameBoard[row][col] != 0) {
+                    System.out.print( gameBoard[row][col] + " " );
+                }
+                else
+                    System.out.print("  ");
+            }
+            System.out.println("");
+        }
+        System.out.println("\n\n");
+    }
+
+    static void clearScreen() {
+
+        for (int i = 0; i < 100; i ++ ) {
+            System.out.println("");
+        }
+    }
+
 }
